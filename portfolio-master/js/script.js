7235d360434ee40ed725cb62dd6078e75d86bd46
conoramanatullah <conor.amanatullah@gmail.com>
2016-04-08 00:48:53 -0600

    lmao
    lmao

--- portfolio-master/js/script.js
+++ portfolio-master/js/script.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
   // Ready all page elements
   $('.materialboxed').materialbox();
   $('.parallax').parallax();
+  $('.modal-trigger').leanModal({
+    dismissible: false,
+    opacity: 0
+  });
+  var clicks = 0;
+  $('#me').click(function(){
+    clicks++;
+    if(clicks == 20){
+      $('#meFace').attr('src','img/ayylmao.jpg')
+    }
+
+  });
+  console.log("HEY YOU. Do you like my website? Send me an email and I'll build you one.")
 })
